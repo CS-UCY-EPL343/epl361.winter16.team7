63f838a0d4aaf6553fc2305be2efe2b788b7b26f
lsolea01 <lsolea01@cs.ucy.ac.cy>
2016-11-17 01:28:08 +0200

    Add files via upload

---
+++ Assets/playvideo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+[RequireComponent(typeof(AudioSource))]
+
+public class playvideo : MonoBehaviour {
+	public MovieTexture movie;
+	private AudioSource audio; 
+
+	void Start () {
+		movie.loop = true; 
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		audio = GetComponent<AudioSource> ();
+		audio.clip = movie.audioClip;
+		movie.Play ();
+		audio.Play ();
+	
+	}
+	
+	 
+	void Update () {
+		
+	
+	}
+}
