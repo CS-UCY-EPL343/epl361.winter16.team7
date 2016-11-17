f8f9a826903c3d83299a631415bb18f7016188c4
cchrys06 <cchrys06@cs.ucy.ac.cy>
2016-11-17 19:33:00 +0200

    Register menu
    Register menu finish

---
+++ eFollowMeGame/Assets/Register/guiControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNextLevel : MonoBehaviour 
+{/*
+	public void OnClick(int index)
+	{
+		Application.LoadLevel(index);
+	}
+
+	public void NextLevelButton(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}*/
+}
