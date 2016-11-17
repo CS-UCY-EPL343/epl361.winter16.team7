a5718da241e8bc161c419ec2fb5fa26edc88901e
cchrys06 <cchrys06@cs.ucy.ac.cy>
2016-11-17 19:01:21 +0200

    Settings Menu done
    Full Screen on/off working.
    Resolution changing working.
    Texture quality chaning working.
    Antialiasing changing working.
    vSync changing working.
    Test Music + music volume added and working

--- eFollowMeGame/Assets/Settings/GameSettings.cs
+++ eFollowMeGame/Assets/Settings/GameSettings.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameSettings : MonoBehaviour {
+public class GameSettings {
+	public bool fullscreen;
+	public int textureQuality;
+	public int antialiasing;
+	public int vSync;
+	public int resolutionIndex;
+	public float musicVolume;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
 }
--- eFollowMeGame/Assets/Settings/SettingManager.cs
+++ eFollowMeGame/Assets/Settings/SettingManager.cs
@@ -1,15 +1,105 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+[System.Serializable]
+public class SettingManager: MonoBehaviour {
+	public Toggle fullscreenToggle;
+	public Dropdown resolutionDropdown;
+	public Dropdown textureQualityDropdown;
+	public Dropdown antialiasingDropdown;
+	public Dropdown vSyncDopwdown;
+	public Slider musicVolumeSlider;
+	public Button applyButton;
 
-public class SettingManager : MonoBehaviour {
+	public AudioSource musicSource;
+	public Resolution[] resolutions;
+	public GameSettings gameSettings;
 
-	// Use this for initialization
-	void Start () {
-	
+	void OnEnable(){
+
+		gameSettings= new GameSettings();
+
+		fullscreenToggle.onValueChanged.AddListener(delegate{OnFullscreenToggle();});
+		resolutionDropdown.onValueChanged.AddListener(delegate{OnResolutionChange();});
+		textureQualityDropdown.onValueChanged.AddListener(delegate{OnTextureQualityChange();});
+		antialiasingDropdown.onValueChanged.AddListener(delegate{OnAntialiasingChange();});
+		vSyncDopwdown.onValueChanged.AddListener(delegate{OnVSyncChange();});
+		musicVolumeSlider.onValueChanged.AddListener(delegate{OnMusicVolumeChange();});
+		applyButton.onClick.AddListener (delegate {OnApplyButtonClick ();});
+
+
+		resolutions = Screen.resolutions; 
+		foreach (Resolution resolution in resolutions) {
+			resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+		
+		}
+		LoadSettings ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void OnFullscreenToggle(){
+
+		gameSettings.fullscreen= Screen.fullScreen = fullscreenToggle.isOn;
+
 	}
+
+	public void OnResolutionChange(){
+		Screen.SetResolution (resolutions [resolutionDropdown.value].width, resolutions [resolutionDropdown.value].height, Screen.fullScreen);
+	}
+
+	public void OnTextureQualityChange(){
+
+		QualitySettings.masterTextureLimit= gameSettings.textureQuality = textureQualityDropdown.value;
+
+	}
+
+	public void OnAntialiasingChange(){
+
+		QualitySettings.antiAliasing = gameSettings.antialiasing=  (int) Mathf.Pow (2f, antialiasingDropdown.value);
+
+	}
+
+	public void OnVSyncChange(){
+		QualitySettings.vSyncCount = gameSettings.vSync = vSyncDopwdown.value;
+
+	}
+
+	public void OnMusicVolumeChange(){
+		musicSource.volume =gameSettings.musicVolume= musicVolumeSlider.value;
+	}
+	public void OnApplyButtonClick(){
+		SaveSettings();
+	}
+	public void SaveSettings(){
+		string jsonData = JsonUtility.ToJson (gameSettings,true);
+	
+		File.WriteAllText (Application.persistentDataPath + "/gamesettings.json", jsonData);
+	}
+
+	public void LoadSettings(){
+		gameSettings = JsonUtility.FromJson<GameSettings> (File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
+		musicVolumeSlider.value = gameSettings.musicVolume;
+		antialiasingDropdown.value = gameSettings.antialiasing;
+		vSyncDopwdown.value = gameSettings.vSync;
+		textureQualityDropdown.value = gameSettings.textureQuality;
+		resolutionDropdown.value = gameSettings.resolutionIndex;
+		fullscreenToggle.isOn = gameSettings.fullscreen;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
