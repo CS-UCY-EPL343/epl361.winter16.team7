18ca11f083d29ce6577f7b6c9bd022b0299988a3
lsolea01 <lsolea01@cs.ucy.ac.cy>
2016-11-21 19:59:23 +0200

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class Login : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject password;
+    private string Username;
+    private string Password;
+    public int a;
+
+
+    public void button()
+    {
+        if (a == 1)
+        {
+            Application.LoadLevel(3);
+
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+        }
+
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Password != "" && Username != "")
+            {
+                Application.LoadLevel(3);
+            }
+
+
+
+        }
+        if (Password != "" && Username != "")
+        {
+            a = 1;
+        }
+
+    }
+}
