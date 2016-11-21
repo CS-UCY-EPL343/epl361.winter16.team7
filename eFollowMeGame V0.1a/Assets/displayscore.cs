4932f716689bbb93216399af31bf23dc7503b9b4
mioann47 <mioann47@cs.ucy.ac.cy>
2016-11-21 21:38:53 +0200

    Updated
    Ingame score
    Project Update

---
+++ eFollowMeGame V0.1a/Assets/displayscore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class displayscore : MonoBehaviour {
+    private GUIStyle guiStyle = new GUIStyle(); //create a new variable
+    // Use this for initialization
+    void OnGUI()
+    {
+        guiStyle.fontSize = 30;
+        // GUI.Label(new Rect(10, 10, 200, 30), "Player Score");
+        GUILayout.Label("Player Score: --", guiStyle);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
