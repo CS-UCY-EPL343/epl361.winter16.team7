9ca7d47a346bddeffd0b263c1f1baeefadf97579
cchrys06 <cchrys06@cs.ucy.ac.cy>
2016-12-01 19:53:54 +0200

    Ingame menu is now bigger

--- eFollowMeGame V0.1a/Assets/Pause Menu/PauseMenu.js
+++ eFollowMeGame V0.1a/Assets/Pause Menu/PauseMenu.js
@@ -6,7 +6,6 @@ private var pauseEnabled = false;
 function Start(){
 	pauseEnabled = false;
 	Time.timeScale = 1;
-	//AudioListener.volume = 1;
 	Cursor.visible = false;
 }
 
@@ -20,14 +19,12 @@ function Update(){
 			//unpause the game
 			pauseEnabled = false;
 			Time.timeScale = 1;
-		//	AudioListener.volume = 1;
 			Cursor.visible = false;			
 		}
 		
 		//else if game isn't paused, then pause it
 		else if(pauseEnabled == false){
 			pauseEnabled = true;
-		//	AudioListener.volume = 0;
 			Time.timeScale = 0;
 			Cursor.visible = true;
 		}
@@ -38,61 +35,25 @@ private var showGraphicsDropDown = false;
 
 function OnGUI(){
 
-GUI.skin.box.font = pauseMenuFont;
-GUI.skin.button.font = pauseMenuFont;
+    GUI.skin.box.font = pauseMenuFont;
+    GUI.skin.box.fontSize = 30;
+    GUI.skin.button.font = pauseMenuFont;
+    GUI.skin.button.fontSize = 30;
 
 	if(pauseEnabled == true){
 		
 		//Make a background box
-		GUI.Box(Rect(Screen.width /2 - 100,Screen.height /2 - 100,250,150), "Pause Menu");
+		GUI.Box(Rect(Screen.width /2 - 250,Screen.height /2 - 100,500,300), "Pause Menu");
 		
 		//Make Main Menu button
-		if(GUI.Button(Rect(Screen.width /2 - 100,Screen.height /2 - 50,250,50), "Back to Main Menu")){
+		if(GUI.Button(Rect(Screen.width /2 - 250,Screen.height /2 ,500,100), "Back to Main Menu")){
 			Application.LoadLevel(mainMenuSceneName);
 		}
-		/*
-		//Make Change Graphics Quality button
-			if(GUI.Button(Rect(Screen.width /2 - 100,Screen.height /2 ,250,50), "Change Graphics Quality")){
-			
-			if(showGraphicsDropDown == false){
-				showGraphicsDropDown = true;
-			}
-			else{
-				showGraphicsDropDown = false;
-			}
-		}
 		
-		//Create the Graphics settings buttons, these won't show automatically, they will be called when
-		//the user clicks on the "Change Graphics Quality" Button, and then dissapear when they click
-		//on it again....
-		if(showGraphicsDropDown == true){
-			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 ,250,50), "Fastest")){
-				QualitySettings.currentLevel = QualityLevel.Fastest;
-			}
-			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 50,250,50), "Fast")){
-				QualitySettings.currentLevel = QualityLevel.Fast;
-			}
-			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 100,250,50), "Simple")){
-				QualitySettings.currentLevel = QualityLevel.Simple;
-			}
-			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 150,250,50), "Good")){
-				QualitySettings.currentLevel = QualityLevel.Good;
-			}
-			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 200,250,50), "Beautiful")){
-				QualitySettings.currentLevel = QualityLevel.Beautiful;
-			}
-			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 250,250,50), "Fantastic")){
-				QualitySettings.currentLevel = QualityLevel.Fantastic;
-			}
-			
-			if(Input.GetKeyDown("escape")){
-				showGraphicsDropDown = false;
-			}
-		}*/
 		
 		//Make quit game button
-		//if (GUI.Button (Rect (Screen.width /2 - 100,Screen.height /2 + 50,250,50), "Quit Game")){
-		if  (GUI.Button(Rect(Screen.width /2 - 100,Screen.height /2 ,250,50), "Quit Game")){
+		
+		if  (GUI.Button(Rect(Screen.width /2 - 250,Screen.height /2 +100,500,100), "Quit Game")){
 			Application.Quit();
 		}
 	}
