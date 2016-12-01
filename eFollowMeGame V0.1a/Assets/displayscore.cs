c0a65c994388ed63c0f4a6b0a6480abb5565be84
mioann47 <mioann47@cs.ucy.ac.cy>
2016-12-01 20:57:54 +0300

    Script updated
    Display score is now bigger
    added info for pressing escape button

--- eFollowMeGame V0.1a/Assets/displayscore.cs
+++ eFollowMeGame V0.1a/Assets/displayscore.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class displayscore : MonoBehaviour {
     private GUIStyle guiStyle = new GUIStyle(); //create a new variable
+    private GUIStyle guiStyle2 = new GUIStyle(); //create a new variable
     // Use this for initialization
     void OnGUI()
     {
-        guiStyle.fontSize = 30;
+        guiStyle.fontSize = 45;
         // GUI.Label(new Rect(10, 10, 200, 30), "Player Score");
-        GUILayout.Label("Player Score: --", guiStyle);
+        GUILayout.Label("Player Score: ---", guiStyle);
+
+        guiStyle2.fixedHeight = 100;
+
+        GUILayout.Label("Press 'ESC' to pause", guiStyle2);
     }
 	
 	// Update is called once per frame
