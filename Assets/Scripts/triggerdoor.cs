742b8e1b0c2ad1ba81c8afda504d10748a6c26d5
mioann47 <mioann47@cs.ucy.ac.cy>
2017-03-16 16:31:02 +0200

    Script for doors
    Script to help player move past doors with wrong built-in collider

---
+++ Assets/Scripts/triggerdoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerdoor : MonoBehaviour {
+	public MeshCollider mc;
+	public GameObject go;
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+	{
+		if(go.GetComponent<Collider>()!=null)
+			Destroy (go.GetComponent<Collider>());
+	}
+	void OnTriggerExit(){
+		if(go.GetComponent<Collider>()==null)
+		go.AddComponent <MeshCollider>();
+	}
+}
