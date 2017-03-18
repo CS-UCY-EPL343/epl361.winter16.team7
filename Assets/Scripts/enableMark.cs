29fbd1a3fabe8702dc510f2c1f343867e144df64
mioann47 <mioann47@cs.ucy.ac.cy>
2017-03-18 02:43:17 +0200

    scripts for marks
    Script to enable/disable marks on map and quest indicators

---
+++ Assets/Scripts/enableMark.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class enableMark : MonoBehaviour {
+
+	public string[] questsNames;
+	public string[] states;
+
+	private string[] returnedStates;
+	string state1;
+	string state0;
+	public GameObject mark;
+	public GameObject exclamationMark;
+	public bool check=true;
+	// Update is called once per frame
+	void Update () {
+
+		int slength = states.Length;
+		returnedStates = new string[slength];
+		for (int i = 0; i < slength; i++) {
+			returnedStates[i]=DialogueLua.GetQuestField(questsNames[i], "State").AsString;
+			if (returnedStates [i] != states [i]) {
+				mark.SetActive (false);
+				exclamationMark.SetActive (false);
+				break;
+			}else {
+				exclamationMark.SetActive (true);
+				mark.SetActive (true);
+				check = true;
+			}
+		}
+
+
+		/**
+		state0 = DialogueLua.GetQuestField("q1", "State").AsString;
+		state1 = DialogueLua.GetQuestField("q2", "State").AsString;
+
+		if (state0 == "success" && state1 == "unassigned") {
+		
+			mark.SetActive (true);
+		} else
+			mark.SetActive (false);
+*/
+
+
+	}
+}
---
+++ Assets/Scripts/enableQuestIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class enableQuestIndicator : MonoBehaviour {
+
+	public string variable;
+	public bool condition;
+	public GameObject obj;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (DialogueLua.GetVariable (variable).AsBool==condition){
+			obj.SetActive (true);
+		}else{
+			obj.SetActive (false);
+		}
+
+	}
+}
