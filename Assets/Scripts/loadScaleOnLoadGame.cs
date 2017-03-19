a645f71ab0b2bb2899b7cd68ddc3081875c7349e
cchrys06 <cchrys06@cs.ucy.ac.cy>
2017-03-19 16:06:53 +0200

    load the scale script
    player scale is now correctly loading from dialogue system

---
+++ Assets/Scripts/loadScaleOnLoadGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class loadScaleOnLoadGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject.FindWithTag ("Player").transform.localScale = 
+			new Vector3 (
+				DialogueLua.GetVariable ("PlayerX").AsFloat,
+				DialogueLua.GetVariable ("PlayerY").AsFloat,
+				DialogueLua.GetVariable ("PlayerZ").AsFloat);
+	}
+	
+
+}
