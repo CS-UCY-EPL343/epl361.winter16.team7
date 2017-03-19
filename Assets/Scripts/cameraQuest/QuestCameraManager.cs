bd942a8905b7f3f1d5323dda4fa9de05eccc7371
cchrys06 <cchrys06@cs.ucy.ac.cy>
2017-03-19 05:26:56 +0200

    Scripts for camera quest
    mp3 sound for camera capture added
    reActive script to flush the screen when capturing
    QuestCameraManager to control the quest/conversations/phone

---
+++ Assets/Scripts/cameraQuest/QuestCameraManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class QuestCameraManager : MonoBehaviour {
+
+	public GameObject Male, Female;
+	public GameObject objectPlayer,other,msg;
+	public GameObject objectCamera;
+	public GameObject finalDisable;
+	public GameObject Message;
+	private float sec = 4f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnTriggerExit(){
+		Message.SetActive (false);
+	}
+	// Update is called once per frame
+	void OnTriggerStay (Collider other) {
+		if (other.tag == "Player" && DialogueLua.GetVariable ("mariaPhoto").AsBool) {
+			msg.SetActive (false);
+			Message.SetActive (true);
+			if (DialogueLua.GetVariable ("mariaPhoto").AsBool && Input.GetButtonDown ("takePhotoG")) {
+				Message.SetActive (false);
+				objectPlayer.SetActive (false);
+				if (DialogueLua.GetVariable ("Male").AsBool) {
+					Female.SetActive (false);
+					Male.SetActive (true);
+
+				} else {
+					Male.SetActive (false);
+					Female.SetActive (true);
+				}
+				objectCamera.SetActive (true);
+				StartCoroutine (continueGame ());
+			}
+		}
+	}
+
+	IEnumerator  continueGame(){
+		yield return new WaitForSeconds(sec);
+		DialogueLua.SetVariable("q4", true);
+		DialogueLua.SetVariable("q4q", true);
+		objectPlayer.SetActive (true);
+		finalDisable.SetActive (false);
+	}
+}
